d5bb0713a6fd52a06ca2ab9dcb0a7cba9e179808
Rayaan Uddin <rayaan.uddin@outlook.com>
2024-08-01 00:09:59 +0100

    Updated database links

--- app/config/const.php
+++ app/config/const.php
@@ -1,16 +1,16 @@
 <?php
 
 // DB Params
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "root");
-define("DB_NAME", "store");
+define("DB_HOST", "rayaanfstore.mysql.db");
+define("DB_USER", "rayaanfstore");
+define("DB_PASS", "Rayaan10");
+define("DB_NAME", "rayaanfstore");
 
 // Stripe API
 define("STRIPE_PUBLISHABLE_KEY", "sk_test_51PTE5aP1xDsj8dmpsZ1XdSW0C1dHbT7J7e2JG3ZI2EZJWB4RvGBKrPl121g2iCxjC3UIw9aEn4vWdRh0iJVRj8n700DuZtuXh7");
 
 // App URL
-define("ROOT_PATH", "/selling-website/app");
+define("ROOT_PATH", "/app");
 define("ROOT_URL", "https://selling-website.rayaanuddin.co.uk");
 
 // Site Name
@@ -20,7 +20,7 @@ define("SITE_NAME", "DAWME INC");
 define("CHECKOUT_SUCCESS_URL", ROOT_URL . ROOT_PATH . "/success.php");
 define("CHECKOUT_CANCEL_URL", ROOT_URL . ROOT_PATH . "/cancel.php");
 
-//product limit
+//product limit per page
 define("PRODUCT_LIMIT", 8);
 
 // Code expire time
